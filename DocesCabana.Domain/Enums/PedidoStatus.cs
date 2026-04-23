160bfc9258da7602b4f17a31ce560e4ad55e3b39
IvanSSantana <ivansantospriv@gmail.com>
2026-04-23 00:33:25 -0300

    Criando camada de domínio do projeto e enums

---
+++ DocesCabana.Domain/Enums/PedidoStatus.cs
@@ -0,0 +1,10 @@
+namespace DocesCabana.Domain.Enums;
+
+public enum PedidoStatus : byte
+{
+    Pendente = 0,
+    Confirmado = 1,
+    Enviado = 2,
+    Entregue = 3,
+    Cancelado = 4
+}
---
+++ DocesCabana.Domain/Enums/PromocaoTipo.cs
@@ -0,0 +1,9 @@
+namespace DocesCabana.Domain.Enums;
+
+public enum PromocaoTipo : byte
+{
+    Percentual   = 0,
+    ValorFixo    = 1,
+    CompreXLeveY = 2,
+    FreteGratis  = 3,
+}
