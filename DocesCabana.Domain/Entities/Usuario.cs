d60d16e7fc3f3523fd2322609a38a8b5b239fb63
IvanSSantana <ivansantospriv@gmail.com>
2026-04-26 18:01:46 -0300

    Criando testes unitários para regras de negócio da entidade Usuário

--- DocesCabana.Domain/Entities/Usuario.cs
+++ DocesCabana.Domain/Entities/Usuario.cs
@@ -1,4 +1,3 @@
-using System.Net.Mail;
 using System.Text.RegularExpressions;
 
 namespace DocesCabana.Domain.Entities;
@@ -22,6 +21,7 @@ public class Usuario
         ValidarNome(nome);
         ValidarEmail(email);
         ValidarSenha(senha);
+        ValidarCelular(celular);
         ValidarCPF(cpf);
 
         Nome = nome;
@@ -43,14 +43,11 @@ public class Usuario
         if (string.IsNullOrWhiteSpace(email))
             throw new ArgumentNullException("Email é obrigatório!");
 
-        try
-        {
-            var addr = new MailAddress(email);
-        }
-        catch
-        {
+        // Regex oficial do HTML5 que segue RFC 5322
+        Regex validacao_regex = new(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)*$");
+
+        if (!validacao_regex.IsMatch(email))
             throw new ArgumentException("Email inválido!");
-        }
     }
 
     private void ValidarSenha(string senha)
@@ -58,12 +55,27 @@ public class Usuario
         if (string.IsNullOrWhiteSpace(senha))
             throw new ArgumentNullException("Senha é obrigatória!");
 
+        // Aceita CPFs já formatadas e somente números
         Regex validacao_regex = new(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$");
 
         if (!validacao_regex.IsMatch(senha))
             throw new ArgumentException(
                 "Senha deve ter no mínimo 6 caracteres, incluindo letra maiúscula, minúscula, número e caractere especial."
             );
+    }
+
+    private void ValidarCelular(string celular)
+    {
+        if (string.IsNullOrWhiteSpace(celular))
+            throw new ArgumentNullException("Celular é obrigatório!");
+
+        // Aceita números com ou sem formatação
+        Regex validacao_regex = new(@"^(?:[14689][1-9]|2[12478]|3[1-5]|3[7-8]|5[1345]|7[134579])9\d{8}$");
+        
+        celular = new string(celular.Where(char.IsDigit).ToArray());
+
+        if (!validacao_regex.IsMatch(celular))
+            throw new ArgumentException("Número de celular inválido!");
     }
 
     private void ValidarCPF(string cpf)
@@ -82,17 +94,17 @@ public class Usuario
 
     private bool CpfValido(string cpf)
     {
-        int[] multiplicador1 = { 10,9,8,7,6,5,4,3,2 };
-        int[] multiplicador2 = { 11,10,9,8,7,6,5,4,3,2 };
+        int[] multiplicador1 = [10,9,8,7,6,5,4,3,2];
+        int[] multiplicador2 = [11,10,9,8,7,6,5,4,3,2];
 
-        var tempCpf = cpf.Substring(0, 9);
-        var soma = tempCpf.Select((t, i) => (t - '0') * multiplicador1[i]).Sum();
+        var primeirosDigitosCPF = cpf.Substring(0, 9);
+        var soma = primeirosDigitosCPF.Select((t, i) => (t - '0') * multiplicador1[i]).Sum();
 
         var resto = soma % 11;
         var digito = resto < 2 ? 0 : 11 - resto;
 
-        tempCpf += digito;
-        soma = tempCpf.Select((t, i) => (t - '0') * multiplicador2[i]).Sum();
+        primeirosDigitosCPF += digito;
+        soma = primeirosDigitosCPF.Select((t, i) => (t - '0') * multiplicador2[i]).Sum();
 
         resto = soma % 11;
         digito = resto < 2 ? 0 : 11 - resto;
