a19b37312003310ab724bf196b0be6337053dd28
IvanSSantana <ivansantospriv@gmail.com>
2026-05-09 09:42:38 -0300

    Completando header principal

---
+++ DocesCabana.MVC/ViewComponents/Header.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocesCabana.MVC.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync(int itensCarrinho = 0, int tipoHeader = 2 )
+        {
+            ViewData["ItensCarrinho"] = itensCarrinho;
+            ViewData["TipoHeader"] = tipoHeader;
+            return Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
