31706356fd0eaa49ae315495721da1717e5a3bf0
IvanSSantana <ivansantospriv@gmail.com>
2026-05-17 17:09:04 -0300

    Adicionando Identity e FluentValidation

---
+++ DocesCabana.Application/Validators/CadastroDTOValidator.cs
@@ -0,0 +1,44 @@
+using FluentValidation;
+using DocesCabana.Application.DTOs.Auth;
+
+namespace DocesCabana.Application.Validators;
+
+public class CadastroDTOValidator : AbstractValidator<CadastroDTO>
+{
+    public CadastroDTOValidator()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty().WithMessage("Nome é obrigatório!")
+            .MaximumLength(100).WithMessage("O nome deve ter no máximo 100 caracteres.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("O E-mail é obrigatório!")
+            .EmailAddress().WithMessage("O E-mail é inválido.")
+            .MaximumLength(100).WithMessage("O E-mail deve ter no máximo 100 caracteres.");
+
+        RuleFor(x => x.Telefone)
+            .NotEmpty().WithMessage("O número de telefone é obrigatório!")
+            .MaximumLength(20).WithMessage("O número de telefone deve ter no máximo 20 caracteres.")
+            .Matches(@"^(?:[14689][1-9]|2[12478]|3[1-5]|3[7-8]|5[1345]|7[134579])9\d{8}$")
+            .WithMessage("Número de telefone inválido.");
+
+        RuleFor(x => x.DataNascimento)
+            .NotEmpty().WithMessage("A data de nascimento é obrigatória!");
+
+        RuleFor(x => x.CPF)
+            .NotEmpty().WithMessage("O CPF é obrigatório!")
+            .MaximumLength(14).WithMessage("O CPF deve ter no máximo 14 caracteres.");
+            
+        RuleFor(x => x.Senha)
+            .NotEmpty().WithMessage("A senha é obrigatória!")
+            .MinimumLength(6).WithMessage("A senha deve ter no mínimo 6 caracteres.")
+            .MaximumLength(50).WithMessage("A senha deve ter no máximo 50 caracteres.")
+            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$")
+            .WithMessage("Senha deve ter no mínimo 6 caracteres, incluindo letra maiúscula, minúscula, número e caractere especial.");
+
+        RuleFor(x => x.ConfirmacaoSenha)
+            .NotEmpty().WithMessage("A confirmação da senha é obrigatória!")
+            .Equal(x => x.Senha).WithMessage("As senhas não coincidem!")
+            .MaximumLength(50).WithMessage("A confirmação da senha deve ter no máximo 50 caracteres.");
+    }
+}
