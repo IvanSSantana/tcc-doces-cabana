28e6e66333503ed0a84500a8f17c7cbfd30457a6
Pedro-Grigolato <helooboarettoo@gmail.com>
2026-05-21 12:26:05 -0300

    Criando arquivos UsuarioServices e ProdutoServices

---
+++ DocesCabana.Application/Services/UsuarioServices.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocesCabana.Application.Contracts.Services
+{
+    public class UsuarioServices
+    {
+        
+    }
+}
