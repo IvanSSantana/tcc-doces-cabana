a1ea17b1caadcfb5e6f41b5771a8d8031ca51cb7
pedro-grigolato <gzpedrao16@gmail.com>
2026-05-26 11:32:25 -0300

    Criando Controller de Autenticação

--- DocesCabana.Application/DTOs/Autenticacao/LoginDTO.cs
+++ DocesCabana.Application/DTOs/Autenticacao/LoginDTO.cs
@@ -1,10 +1,10 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace DocesCabana.Application.DTOs.Auth;
+namespace DocesCabana.Application.DTOs.Autenticacao;
 
 public class LoginDTO
 {
     public string Login { get; set; } = string.Empty; // Pode ser e-mail ou telefone
 
     public string Senha { get; set; } = string.Empty;
+    
+    public bool LembrarMe { get; set; } = false;
 }
